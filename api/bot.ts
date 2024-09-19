b94a70dfee2b5dc26e1124eb7d12d394068d2d49
gfirik <firdavs.gafurjonov@gmail.com>
2024-09-20 07:26:52 +0900

    fix: configs for deployment, try 1

--- api/bot.ts
+++ api/bot.ts
@@ -2,11 +2,11 @@ import { webhookCallback } from "grammy";
 import { serve } from "bun";
 import bot from "../src/bot/main";
 
-const handleUpdate = webhookCallback(bot, "http");
+const handleUpdate = webhookCallback(bot, "std/http");
 
 serve({
+  port: 6666,
   fetch(req) {
     return handleUpdate(req);
   },
-  port: 6666,
 });
