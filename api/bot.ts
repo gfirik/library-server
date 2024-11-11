ec7b0f2bf4ec493801a068c6a297573c91a838c1
gfirik <firdavs.gafurjonov@gmail.com>
2024-11-11 21:26:59 +0900

    logs implemented for test

--- api/bot.ts
+++ api/bot.ts
@@ -6,9 +6,11 @@ const handler = webhookCallback(bot, "std/http");
 export default async function (req: Request) {
   if (req.method === "POST") {
     try {
+      console.log("Webhook received a request! Processing... \n");
       await handler(req);
       return new Response("OK", { status: 200 });
     } catch (error) {
+      console.error("Error handling webhook:", error);
       return new Response("Error handling the webhook", { status: 500 });
     }
   }
