d3f78c882106f38217ca4d38141fd3ccf6dbf0c2
gfirik <firdavs.gafurjonov@gmail.com>
2024-11-25 18:37:22 +0900

    files moved to api folder & tsconfig file changed

--- api/bot.ts
+++ api/bot.ts
@@ -1,25 +1,12 @@
 import { webhookCallback } from "grammy";
 import { run } from "@grammyjs/runner";
-import bot from "../src/bot/main.js";
-import env from "../src/utils/env.js";
+import bot from "./src/bot/main.js";
+import env from "./src/utils/env.js";
 
-if (env.NODE_ENV === "production") {
-  const handler = webhookCallback(bot, "std/http");
+export default webhookCallback(bot, "std/http");
+console.log("Webhook server running...");
 
-  Bun.serve({
-    async fetch(req) {
-      if (req.method === "POST") {
-        return await handler(req);
-      } else {
-        return new Response("Method Not Allowed", { status: 405 });
-      }
-    },
-    port: 3333,
-    development: false,
-  });
-
-  console.log("Webhook server running on port 3333");
-} else {
+if (env.NODE_ENV === "development") {
   run(bot);
   console.log("Bot started locally. For testing, ensure to connect via ngrok.");
 }
