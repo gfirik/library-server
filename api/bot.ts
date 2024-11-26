ae3281a95e1b61a9e71ec3babe44a850ba3547b9
gfirik <firdavs.gafurjonov@gmail.com>
2024-11-27 08:41:34 +0900

    webhookcallback function simplified

--- api/bot.ts
+++ api/bot.ts
@@ -4,20 +4,7 @@ import { run } from "@grammyjs/runner";
 import bot from "./src/bot/main.js";
 import env from "./src/utils/env.js";
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  if (req.method === "POST") {
-    try {
-      const handler = webhookCallback(bot, "https");
-      await handler(req, res);
-    } catch (error) {
-      console.error("Error handling webhook:", error);
-      res.status(500).send("Internal Server Error");
-    }
-  } else {
-    res.status(405).send("Method Not Allowed");
-  }
-}
-
+export default webhookCallback(bot, "std/http");
 console.log("Webhook server running...");
 
 if (env.NODE_ENV === "development") {
