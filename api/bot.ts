527bd60c2149f1d2357c74fbc7a5066675e1d18f
gfirik <firdavs.gafurjonov@gmail.com>
2024-11-27 08:42:28 +0900

    if line change

--- api/bot.ts
+++ api/bot.ts
@@ -4,10 +4,10 @@ import { run } from "@grammyjs/runner";
 import bot from "./src/bot/main.js";
 import env from "./src/utils/env.js";
 
-export default webhookCallback(bot, "std/http");
-console.log("Webhook server running...");
-
 if (env.NODE_ENV === "development") {
   run(bot);
   console.log("Bot started locally. For testing, ensure to connect via ngrok.");
 }
+
+export default webhookCallback(bot, "std/http");
+console.log("Webhook server running...");
