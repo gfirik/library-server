501f1f2ac1da732d0ead65efdb2b135701c21152
gfirik <firdavs.gafurjonov@gmail.com>
2024-11-27 09:17:00 +0900

    cleanup for deployment

--- api/bot.ts
+++ api/bot.ts
@@ -1,13 +1,4 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { webhookCallback } from "grammy";
 import { run } from "@grammyjs/runner";
 import bot from "./src/bot/main.js";
-import env from "./src/utils/env.js";
 
 run(bot);
-// if (env.NODE_ENV === "development") {
-//   console.log("Bot started locally. For testing, ensure to connect via ngrok.");
-// }
-
-// export default webhookCallback(bot, "std/http");
-// console.log("Webhook server running...");
